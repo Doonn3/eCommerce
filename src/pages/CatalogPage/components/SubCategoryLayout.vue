19eceebe6b50b74a21b2ef898fb1f74232d5985a
Николай <Doonn3@gmail.com>
2023-10-08 12:17:21 +0300

    feat: add catalog page and base home page

---
+++ src/pages/CatalogPage/components/SubCategoryLayout.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { useCategoryStore } from '@entities/Categories';
+
+const categoryStore = useCategoryStore();
+</script>
+
+<template>
+  <section></section>
+</template>
