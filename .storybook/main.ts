84cdcd5d6bc363fa6695d27c6fd7a827d72909e7
Николай <Doonn3@gmail.com>
2023-11-18 12:07:41 +0300

    refactor: storybook main

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,13 +3,8 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 const config: StorybookConfig = {
   // stories: ['../src/shared/stories/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-vue-slots',
-    "storybook-addon-data-theme-switcher",
-  ],
+  // addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-addon-vue-slots', 'storybook-addon-data-theme-switcher'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/vue3-vite',
     options: {}
@@ -20,5 +15,5 @@ const config: StorybookConfig = {
   core: {
     builder: '@storybook/builder-vite'
   }
-};
+} satisfies StorybookConfig ;
 export default config;
