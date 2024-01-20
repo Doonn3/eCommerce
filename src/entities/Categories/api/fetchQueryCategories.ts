0bd28e36a8fe1e4f20829b0f5f030cf9fb66d483
Doonn <Doonn3@gmail.com>
2024-01-20 19:53:22 +0300

    refactor: entiti categories api

--- src/entities/Categories/api/fetchQueryCategories.ts
+++ src/entities/Categories/api/fetchQueryCategories.ts
@@ -1,19 +1,16 @@
-import { config } from '../../../shared/api/config/cmConfig';
+import { config } from '@/shared/api/config/cmConfig';
 import type { CategoryPagedQueryResponseType } from '../types/CategoryPagedQueryResponseType';
-import { http } from '../../../auth/model/interceptors';
+import { http } from '@/auth';
 
 const { apiUrl, project_key } = config;
 
 const URL = `${apiUrl}/${project_key}/categories`;
-// const URL = `${apiUrl}/${project_key}/categories/search?filter=categories.id:"8ce96d24-0f50-44a6-9124-49bc51a2e80a"`;
-
-// const SCOPES = `${scope.view_products} ${scope.view_categories}`;
 
 export async function fetchQueryCategories() {
   try {
     const res = await http.get<CategoryPagedQueryResponseType>(URL);
-    const resOk = res.status === 200;
-    if (resOk) {
+    const resOK = res.status >= 200 && res.status < 300;
+    if (resOK) {
       return res.data;
     }
     throw new Error();
@@ -21,9 +18,3 @@ export async function fetchQueryCategories() {
     return error as Error;
   }
 }
-
-// export async function fetchQueryCategories() {
-//   const { data, isError, isLoading } = await useFetch<CategoryPagedQueryResponseType>({ url: URL });
-
-//   return { isError, isLoading, data };
-// }
